709f905f4bab4b68f6867b148f7405915ef7e411
Lucas <lucasrosadar@gmail.com>
2022-12-01 11:55:13 -0300

    add description and change name

--- 3 - prime numbers.py
+++ 3 - prime numbers.py
@@ -1,16 +1,14 @@
+# returns the prime numbers of the number 600851475143
+
 z = 600851475143
 
 for x in range(2,10000000):
     flag=0
     if(z!=1):
-        #print("x= " + str(x))
         for y in range(2,x):
-            #print("y= " + str(y))
             if x % y == 0:
-                #print("a")
                 flag=1
                 break
-        #print("abc")
         if(flag!=1):
             while z % x == 0:
                 z = z / x
